7ab2aca649afc2ae45f69557c335f6dcaad62dc7
Mittal <shmittal@ciena.com>
2026-01-29 16:08:01 +0530

    Multithreading

---
+++ src/multithreading/deadlock.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class deadlock {
+
+	public static void main(String[] args) {
+		/*deadlock conditions
+		 * mutual exclusion
+		 * hold and wait
+		 * No preemption
+		 *  Circular wait 
+		 */
+
+	}
+
+}
